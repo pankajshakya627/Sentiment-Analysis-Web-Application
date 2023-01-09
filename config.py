f45499a7e204d0bdad71129c418b8fb63449c8be
Pankaj Shakya <39346757+pankajshakya627@users.noreply.github.com>
2023-01-09 22:28:44 +0530

    Added database and config file
    Added sqlite database and twitter config file

---
+++ config.py
@@ -0,0 +1,4 @@
+api_key = 'QWpBlpKjBuU5yJg4VTlV1uysD'
+api_key_secret  ='tLneAHLhQgnDZmzgrgJfglQGVPlt5Pow3bmAiyL2AfYzM0CSTD'
+access_token ='790449782141575168-TVmavuz77ppbpABH1hjpNobqa5l7KbT'
+access_token_secret ='1nehWIFmCsctVMUlLZbLBQQlIbI0xDaIX9XAqLqeq2TD0'
